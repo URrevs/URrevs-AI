02875af1af5df9834d882dc29989c3dcc4d520eb
MohamedAhmed412000 <memoahmed412000@gmail.com>
2022-05-01 20:53:34 +0300

    add more apps

---
+++ recommenderApi/recommenderApi/FileData.py
@@ -0,0 +1,64 @@
+from recommenderApi.imports import pd, Tuple
+
+class FileData:
+    def __init__(self, file_path: str = '', alert: bool = False) -> None:
+        ''' 
+            show alert message if error is happend
+            load file if file_path is not empty
+
+            parameters: file_path, alert
+            output: None
+        '''
+        self.file_path: str = file_path
+        self.alert: bool = alert
+        try:
+            file = pd.ExcelFile(file_path)
+            self.sheet_names: int = file.sheet_names
+            if self.alert: print('file loaded successfully')
+        except:
+            self.sheet_names, self.file_path = [], ''
+            if self.alert: print("Error: File not found")
+
+    def load_sheet(self, sheet_name: str) -> Tuple[pd.DataFrame, bool]:
+        '''
+            function to load sheet from file
+
+            parameters: sheet_name
+            output: return dataframe of sheet and boolean value represent if sheet is loaded successfully
+        '''
+        try:
+            self.data = pd.read_excel(self.file_path, sheet_name, na_values='n/a')
+            return self.data, True
+        except:
+            if self.alert: print("Error: Sheet not found")
+            return pd.DataFrame(), False
+
+    def get_sheet_names(self) -> list:
+        '''
+            function to get list of sheet names
+
+            parameters: None
+            output: return list of sheet names
+        '''
+        return self.sheet_names
+    
+    def save_sheet(self, sheet_name: str, file_path: str) -> None:
+        '''
+            function to save sheet to file
+
+            parameters: sheet_name, file_path
+            output: None
+        '''
+        try:
+            self.data.to_excel(file_path, sheet_name)
+            if self.alert: print("Sheet saved successfully")
+        except:
+            if self.alert: print("Error: Sheet not found")
+
+    def __str__(self):
+        return self.file_name
+
+# fh = FileData('mobiles.xlsx')
+# # print(fh.get_sheet_names())
+# data = fh.load_sheet('Sheet1')
+# print(data.head(10))
---
+++ recommenderApi/recommenderApi/imports.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from datetime import datetime as dt
+from typing import Tuple
+from sklearn.preprocessing import OneHotEncoder
+from pickle import dump, load
