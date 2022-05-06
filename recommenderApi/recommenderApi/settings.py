1844a3a7885c107dda960aced39dd6c9ff563363
MohamedAhmed412000 <memoahmed412000@gmail.com>
2022-05-06 15:37:34 +0300

    fix settings file

--- recommenderApi/recommenderApi/settings.py
+++ recommenderApi/recommenderApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q5mgy-b5#75ag8@^-m99vf1+@l2@%-het)y6_mp6llsorr=y8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv("API_HOST")]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv("API_HOST"), 'https://urrevs-ai-dev.herokuapp.com']
 
 
 # Application definition
