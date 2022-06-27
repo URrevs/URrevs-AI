2272722032ff3cc1d8893f415fe5893543ec4de7
Mohamed Ahmed <memoahmed412000@gmail.com>
2022-06-27 06:25:48 +0300

    fix mongo client

--- recommenderApi/recommender/mongoDB/getData.py
+++ recommenderApi/recommender/mongoDB/getData.py
@@ -10,7 +10,9 @@ class MongoConnection:
         return None
     
     def connect_to_mongo(self, mongodb_link: str = MONGODB_LINK, mongodb_name: str = MONGODB_NAME):
-        client = MongoClient(mongodb_link, tlsCAFile=certifi.where())
+        print(mongodb_link)
+        # client = MongoClient(mongodb_link, tlsCAFile=certifi.where())
+        client = MongoClient(mongodb_link)
         self.db = client[mongodb_name]
         return self.db
     
