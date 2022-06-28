d0ba7212a7d1f7b58b91f23cac795793769aee43
Mohamed Ahmed <memoahmed412000@gmail.com>
2022-06-28 11:18:17 +0300

    check async reading vars.pkl

--- recommenderApi/recommender/asyn_tasks/tasks.py
+++ recommenderApi/recommender/asyn_tasks/tasks.py
@@ -22,12 +22,15 @@ def start_async(date, first):
     print('check try block')
     try: 
         vars = load(open('recommenderApi/vars.pkl', 'rb'))
+        print(f"(try block) time is: {vars['date']}")
     except:
         vars = {}
+        print('(except block) vars is empty')
+
     print('end try block')
     dump(vars, open('recommenderApi/vars.pkl', 'wb'))
     print('dump file')
-    train_and_update(dt.fromisoformat(date), first=first)
+    # train_and_update(dt.fromisoformat(date), first=first)
     print('end async task')
     return
 
