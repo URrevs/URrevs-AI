7a416bafd25da46374f79948af5230f996686d02
Mohamed Ahmed <memoahmed412000@gmail.com>
2022-06-28 11:47:00 +0300

    fix async for raining

--- recommenderApi/recommender/asyn_tasks/tasks.py
+++ recommenderApi/recommender/asyn_tasks/tasks.py
@@ -17,20 +17,9 @@ from recommender.sqliteDB.data import SQLite_Database
 
 @shared_task
 def start_async(date, first):
-    print('start async task')
     print(dt.fromisoformat(date))
-    print('check try block')
-    try: 
-        vars = load(open('recommenderApi/vars.pkl', 'rb'))
-        print(f"(try block) time is: {vars['date']}")
-    except:
-        vars = {}
-        print('(except block) vars is empty')
-
-    print('end try block')
-    dump(vars, open('recommenderApi/vars.pkl', 'wb'))
-    print('dump file')
-    # train_and_update(dt.fromisoformat(date), first=first)
+    print('start async task')
+    train_and_update(dt.fromisoformat(date), first=first)
     print('end async task')
     return
 
