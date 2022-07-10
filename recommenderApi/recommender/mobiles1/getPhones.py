c53c3105cdd94c494c8da4103adc7ee5668c4666
Mohamed Ahmed <memoahmed412000@gmail.com>
2022-07-10 20:11:00 +0300

    update list

--- recommenderApi/recommender/mobiles1/getPhones.py
+++ recommenderApi/recommender/mobiles1/getPhones.py
@@ -142,9 +142,9 @@ class Similar_Phones:
             external = self.constraints
         for spec in ['price', 'releaseDate', 'company', 'dimensions', 'batteryCapacity', 'os', 'weight',
                 'hasFastCharging', 'screenSize', 'screen2bodyRatio', 'screenResolution', 'usbVersion',
-                'resolutionDensity', 'intMem', 'mainCam', 'selfieCam', 'hasLoudspeaker', 'hasStereo',
-                'has3p5mm', 'hasNfc', 'hasGyro', 'hasProximity', 'bluetoothVersion', 'usbType']:
-                # , 'network', 'screenType', 'usbVersion', 'cpu', 'gpu']:
+                'resolutionDensity', 'mainCam', 'hasLoudspeaker', 'hasStereo',
+                'has3p5mm', 'hasNfc', 'hasGyro', 'hasProximity', 'bluetoothVersion']:
+                # , 'intMem', 'selfieCam', 'usbType', 'network', 'screenType', 'usbVersion', 'cpu', 'gpu']:
             # --------------------------------------------------------------------------------------------
             # NUMERIC DATA
             if spec == 'price' or spec == 'batteryCapacity' or spec == 'weight' or spec == 'screenSize'\
